357fdebedd6fc5fec8aa968bf47b08d72e35a337
mypri <myprinciples40@gmail.com>
2023-07-12 16:58:49 +1000

    #6 - Start spring boot project
    Initialising a Spring Boot project
    the early dependencies follows to go: 
    
    * spring web
    * actuators
    * devtools
    * lombok

---
+++ folder/projects/Course Project/Bulletin board service/Fastcampus-board-service-project/src/test/java/com/fastcampus/boardproject/FastCampusBoardServiceProjectApplicationTests.java
@@ -0,0 +1,14 @@
+package com.fastcampus.boardproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FastCampusBoardServiceProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+
+	}
+
+}
