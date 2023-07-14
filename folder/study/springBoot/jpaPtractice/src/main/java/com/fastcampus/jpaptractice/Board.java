e80eee3922153695cf9d6f7ff13fc489ccd78c40
mypri <myprinciples40@gmail.com>
2023-07-14 21:31:15 +1000

    Test CrudRepository interface

---
+++ folder/study/springBoot/jpaPtractice/src/main/java/com/fastcampus/jpaptractice/Board.java
@@ -0,0 +1,88 @@
+package com.fastcampus.jpaptractice;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Entity
+public class Board {
+    @Id
+    @GeneratedValue //Automatic numbering
+    private Long bno; //Post number
+    private String title;
+    private String Writer;
+    private String content;
+    private Long viewCnt;
+    @Temporal(value = TemporalType.TIMESTAMP) //Date and time mapping
+    private Date inDate;
+    @Temporal(value = TemporalType.TIMESTAMP)
+    private Date upDate;
+
+    @Override
+    public String toString() {
+        return "Board{" +
+                "bno=" + bno +
+                ", title='" + title + '\'' +
+                ", Writer='" + Writer + '\'' +
+                ", content='" + content + '\'' +
+                ", viewCnt=" + viewCnt +
+                ", inDate=" + inDate +
+                ", upDate=" + upDate +
+                '}';
+    }
+
+    public Long getBno() {
+        return bno;
+    }
+
+    public void setBno(Long bno) {
+        this.bno = bno;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getWriter() {
+        return Writer;
+    }
+
+    public void setWriter(String writer) {
+        Writer = writer;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public Long getViewCnt() {
+        return viewCnt;
+    }
+
+    public void setViewCnt(Long viewCnt) {
+        this.viewCnt = viewCnt;
+    }
+
+    public Date getInDate() {
+        return inDate;
+    }
+
+    public void setInDate(Date inDate) {
+        this.inDate = inDate;
+    }
+
+    public Date getUpDate() {
+        return upDate;
+    }
+
+    public void setUpDate(Date upDate) {
+        this.upDate = upDate;
+    }
+}
---
+++ folder/study/springBoot/jpaPtractice/src/test/java/com/fastcampus/jpaptractice/BoardRepositoryTest.java
@@ -0,0 +1,77 @@
+package com.fastcampus.jpaptractice;
+
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.Date;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * Purpose: CrudRepository test
+ * Features: Verify that INSERT, SELECT, UPDATE, and DELETE all work well
+ *
+ * Author: Jinhwan Kim (Jin)
+ * Date created: 2023-07-14
+ * Modification Date:
+ */
+
+@SpringBootTest
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+class BoardRepositoryTest {
+    @Autowired
+    private BoardRepository boardRepo;
+
+    @Test
+    @Order(4)
+    public void deleteTest() {
+        boardRepo.deleteById(1L); //Delete post 1
+
+        Board board = boardRepo.findById(1L).orElse(null);
+        assertTrue(board == null); //Must be null because it was deleted
+    }
+
+    @Test
+    @Order(3)
+    public void updateTest() {
+        Board board = boardRepo.findById(1L).orElse(null);
+        assertTrue(board != null);
+
+        board.setTitle("modified Title");
+        boardRepo.save(board);
+        //못 찾으면 새로운 board객체 반환
+        Board board2 = boardRepo.findById(1L).orElse(new Board());
+        System.out.println("board = " + board);
+        System.out.println("board2 = " + board2);
+
+        assertTrue(board.getTitle().equals(board2.getTitle()));
+    }
+
+    @Test
+    @Order(2)
+    public void selectTest() {
+//        Board board = boardRepo.findById(1L).get(); //Throwing an exception when no value exists
+        Board board = boardRepo.findById(1L).orElse(null); //Returning null when there is no value
+        System.out.println("board = " + board);
+        assertTrue(board != null);
+
+    }
+
+    @Test
+    @Order(1)
+    public void insertTest() {
+        Board board = new Board();
+        board.setBno(1L);
+        board.setTitle("Test Title");
+        board.setContent("This is the Test");
+        board.setWriter("aaa");
+        board.setViewCnt(0L);
+        board.setInDate(new Date());
+        board.setUpDate(new Date());
+        boardRepo.save(board);
+    }
+}
