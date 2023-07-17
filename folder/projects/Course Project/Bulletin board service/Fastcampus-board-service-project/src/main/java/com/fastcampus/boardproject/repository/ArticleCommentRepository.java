19dd499ecd9be6830e56ba8e52db21fd4224ca66
mypri <myprinciples40@gmail.com>
2023-07-17 15:47:54 +1000

    #1 - Set data rest
    Make the JSON API for posts, comments automatically RESTful

--- folder/projects/Course Project/Bulletin board service/Fastcampus-board-service-project/src/main/java/com/fastcampus/boardproject/repository/ArticleCommentRepository.java
+++ folder/projects/Course Project/Bulletin board service/Fastcampus-board-service-project/src/main/java/com/fastcampus/boardproject/repository/ArticleCommentRepository.java
@@ -2,6 +2,10 @@ package com.fastcampus.boardproject.repository;
 
 import com.fastcampus.boardproject.domain.ArticleComment;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.data.rest.webmvc.RepositoryRestController;
 
+// Add and annotate rest in application.yaml
+@RepositoryRestResource
 public interface ArticleCommentRepository extends JpaRepository<ArticleComment, Long> {
 }
