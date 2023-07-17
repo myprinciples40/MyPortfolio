d2b16ed0fe567c0085324ae271e1e7ad2b3dc4ba
mypri <myprinciples40@gmail.com>
2023-07-17 22:06:35 +1000

    Recreate jpa-practice folder
    Add a project again due to an error

---
+++ folder/study/springBoot/jpa-practice/src/main/java/com/fastcampus/jpapractice/User.java
@@ -0,0 +1,76 @@
+package com.fastcampus.jpapractice;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import java.util.Date;
+
+@Entity
+public class User {
+    @Id // Designate as PK
+    private String id;
+    private String password;
+    private String name;
+    private String email;
+    private Date inDate; // Date entered
+    private Date upDate; // Change Date
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id='" + id + '\'' +
+                ", password='" + password + '\'' +
+                ", name='" + name + '\'' +
+                ", email='" + email + '\'' +
+                ", inDate=" + inDate +
+                ", upDate=" + upDate +
+                '}';
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Date getInDate() {
+        return inDate;
+    }
+
+    public void setInDate(Date inDate) {
+        this.inDate = inDate;
+    }
+
+    public Date getUpDate() {
+        return upDate;
+    }
+
+    public void setUpDate(Date upDate) {
+        this.upDate = upDate;
+    }
+}
