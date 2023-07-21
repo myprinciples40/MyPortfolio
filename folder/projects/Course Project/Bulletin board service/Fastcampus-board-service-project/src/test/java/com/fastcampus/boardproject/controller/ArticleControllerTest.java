e8d3bb639ebcf37b307b6b6d33fb74c5f4d74741
mypri <myprinciples40@gmail.com>
2023-07-21 22:18:09 +1000

    #22 - Updating controller tests
    Since I checked the media type checking method in 47dc428, I expect the rest of the views to work the same way and update the test method ahead of time.

--- folder/projects/Course Project/Bulletin board service/Fastcampus-board-service-project/src/test/java/com/fastcampus/boardproject/controller/ArticleControllerTest.java
+++ folder/projects/Course Project/Bulletin board service/Fastcampus-board-service-project/src/test/java/com/fastcampus/boardproject/controller/ArticleControllerTest.java
@@ -51,7 +51,7 @@ class ArticleControllerTest {
         // When & Then
         mvc.perform(get("/articles/1"))
                 .andExpect(status().isOk())
-                .andExpect(content().contentType(MediaType.TEXT_HTML))
+                .andExpect(content().contentTypeCompatibleWith(MediaType.TEXT_HTML))
                 .andExpect(view().name("articles/detail"))
                 .andExpect(model().attributeExists("article"))
                 .andExpect(model().attributeExists("articleComments"));
@@ -66,7 +66,7 @@ class ArticleControllerTest {
         // When & Then
         mvc.perform(get("/articles/search"))
                 .andExpect(status().isOk())
-                .andExpect(content().contentType(MediaType.TEXT_HTML))
+                .andExpect(content().contentTypeCompatibleWith(MediaType.TEXT_HTML))
                 .andExpect(model().attributeExists("articles/search"));
     }
 
@@ -79,7 +79,7 @@ class ArticleControllerTest {
         // When & Then
         mvc.perform(get("/articles/search-hashtag"))
                 .andExpect(status().isOk())
-                .andExpect(content().contentType(MediaType.TEXT_HTML))
+                .andExpect(content().contentTypeCompatibleWith(MediaType.TEXT_HTML))
                 .andExpect(model().attributeExists("articles/search-hashtag"));
     }
 }
