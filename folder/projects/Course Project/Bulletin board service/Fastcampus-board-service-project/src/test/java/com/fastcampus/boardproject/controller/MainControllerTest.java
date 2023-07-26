de309b6166a1777c081dc1f14db10aad00e1b9ed
mypri <myprinciples40@gmail.com>
2023-07-26 19:10:14 +1000

    #31 - Update a controller to forward the root path
    Committed without modifying the redirect to forward

--- folder/projects/Course Project/Bulletin board service/Fastcampus-board-service-project/src/test/java/com/fastcampus/boardproject/controller/MainControllerTest.java
+++ folder/projects/Course Project/Bulletin board service/Fastcampus-board-service-project/src/test/java/com/fastcampus/boardproject/controller/MainControllerTest.java
@@ -3,13 +3,14 @@ package com.fastcampus.boardproject.controller;
 import com.fastcampus.boardproject.config.SecurityConfig;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.ImportAutoConfiguration;
+
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.context.annotation.Import;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @Import(SecurityConfig.class)
 @WebMvcTest(MainController.class)
@@ -22,11 +23,14 @@ class MainControllerTest {
     }
 
     @Test
-    void givenNothing_whenRequestingRootPage_thenRedirectsToArticlePage() throws Exception {
+    void givenNothing_whenRequestingRootPage_thenForwardsToArticlePage() throws Exception {
         // Given
 
         // When & Then
         mvc.perform(get("/"))
-                .andExpect(status().is3xxRedirection());
+                .andExpect(status().isOk())
+                .andExpect(view().name("forward:/articles"))
+                .andExpect(forwardedUrl("/articles"))
+                .andDo(MockMvcResultHandlers.print());
     }
 }
