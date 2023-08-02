3889eee34ec23803d61da55d4fa5e40103c31430
mypri <myprinciples40@gmail.com>
2023-08-02 17:28:49 +1000

    #38 - Implementing and testing the Pagination Service
    Designed a spring service bean to return a list of numbers to use in the page bar when I put in the current page and the total number of pages.

---
+++ folder/projects/Course Project/Bulletin board service/Fastcampus-board-service-project/src/test/java/com/fastcampus/boardproject/service/PaginationServiceTest.java
@@ -0,0 +1,67 @@
+package com.fastcampus.boardproject.service;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
+import java.util.stream.Stream;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.params.provider.Arguments.arguments;
+
+@DisplayName("Business Logic Test for Pagination")
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.NONE, classes = PaginationService.class)
+class PaginationServiceTest {
+    private final PaginationService sut;
+
+    PaginationServiceTest(@Autowired PaginationService paginationService) {
+        this.sut = paginationService;
+    }
+
+    @DisplayName("If you give it the current page number and the total number of pages, it will create a list of paging bars.")
+    @MethodSource
+    @ParameterizedTest(name = "[{index}] Current page: {0}, Total pages: {1} -> {2}")
+    void givenCurrentPageNumberAndTotalPages_whenCalculating_thenReturnsPaginationBarNumbers(int currentPageNumber, int totalPages, List<Integer> expected) {
+        // given
+
+        // when
+        List<Integer> actual = sut.getPaginationBarNumbers(currentPageNumber, totalPages);
+
+        // then
+        assertThat(actual).isEqualTo(expected);
+    }
+
+    static Stream<Arguments> givenCurrentPageNumberAndTotalPages_whenCalculating_thenReturnsPaginationBarNumbers() {
+        return Stream.of(
+                arguments(0, 13, List.of(0, 1, 2, 3, 4)),
+                arguments(1, 13, List.of(0, 1, 2, 3, 4)),
+                arguments(2, 13, List.of(0, 1, 2, 3, 4)),
+                arguments(3, 13, List.of(1, 2, 3, 4, 5)),
+                arguments(4, 13, List.of(2, 3, 4, 5, 6)),
+                arguments(5, 13, List.of(3, 4, 5, 6, 7)),
+                arguments(6, 13, List.of(4, 5, 6, 7, 8)),
+                arguments(10, 13, List.of(8, 9, 10, 11, 12)),
+                arguments(11, 13, List.of(9, 10, 11, 12)),
+                arguments(12, 13, List.of(10, 11, 12))
+        );
+    }
+
+    @DisplayName("Tells you the length of the pagination bar that is currently set.")
+    @Test
+    void givenNothing_whenCalling_thenReturnsCurrentBarLength() {
+        // given
+
+        // when
+        int barLength = sut.currentBarLength();
+
+        // then
+        assertThat(barLength).isEqualTo(5);
+    }
+
+
+}
