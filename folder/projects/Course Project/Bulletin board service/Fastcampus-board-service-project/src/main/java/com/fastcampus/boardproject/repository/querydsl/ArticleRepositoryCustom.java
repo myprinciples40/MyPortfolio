16c8bfe04bb6a3cfafe2540969f9d4c43b06b4d8
mypri <myprinciples40@gmail.com>
2023-08-24 21:08:56 +1000

    #66 - Reflecting domain changes: Hashtags
    Hashtag entities are added, and the design is changed to allow posts to have multiple hashtags.
    However, the test currently fails because it only reflects the domain change and does not implement the functionality, so it only briefly avoids compilation errors.

--- folder/projects/Course Project/Bulletin board service/Fastcampus-board-service-project/src/main/java/com/fastcampus/boardproject/repository/querydsl/ArticleRepositoryCustom.java
+++ folder/projects/Course Project/Bulletin board service/Fastcampus-board-service-project/src/main/java/com/fastcampus/boardproject/repository/querydsl/ArticleRepositoryCustom.java
@@ -1,7 +1,19 @@
 package com.fastcampus.boardproject.repository.querydsl;
 
+import com.fastcampus.boardproject.domain.Article;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+import java.util.Collection;
 import java.util.List;
 
 public interface ArticleRepositoryCustom {
+
+    /**
+     * @deprecated Since I created a new hashtag domain, I don't need to use this code anymore.
+     * @see HashtagRepositoryCustom#findAllHashtagNames()
+     */
+    @Deprecated
     List<String> findAllDistinctHashtags();
+    Page<Article> findByHashtagNames(Collection<String> hashtagNames, Pageable pageable);
 }
